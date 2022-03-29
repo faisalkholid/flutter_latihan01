fe77f2a65125092001024ee12092a80226d526a4
faisalkholid <noah.faisal87@gmail.com>
2022-03-30 07:21:10 +0800

    change code

--- lib/main.dart
+++ lib/main.dart
@@ -11,113 +11,110 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white10, //<-- SEE HERE
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Stack(
-          children: [
-            Container(
-              color: Colors.black87,
-            ),
-            SingleChildScrollView(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 200,
-                  ),
-                  Container(
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 50,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Container(
-                    child: Text(
-                      "Please login to continue life",
-                      style: TextStyle(
-                          color: Colors.white38,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w400),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                          prefixIcon: Icon(
-                            Icons.supervised_user_circle,
-                            color: Colors.white,
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                  width: 1.5, color: Colors.white),
-                              borderRadius: BorderRadius.circular(10)),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(width: 1.5, color: Colors.amber),
-                              borderRadius: BorderRadius.circular(10)),
-                          border: OutlineInputBorder(),
-                          hintText: "User name",
-                          hintStyle: TextStyle(color: Colors.white30)),
-                      controller: TextEditingController(),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: TextField(
-                      obscureText: true,
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                          prefixIcon: Icon(
-                            Icons.remove_red_eye_rounded,
-                            color: Colors.white,
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                  width: 1.5, color: Colors.white),
-                              borderRadius: BorderRadius.circular(10)),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(width: 1.5, color: Colors.amber),
-                              borderRadius: BorderRadius.circular(10)),
-                          border: OutlineInputBorder(),
-                          hintText: "*********",
-                          hintStyle: TextStyle(color: Colors.white30)),
-                      controller: TextEditingController(),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Container(
-                    height: 60,
-                    width: 400,
-                    margin: EdgeInsets.symmetric(horizontal: 20),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: GestureDetector(
-                        child: TextButton(
-                          onPressed: () {},
-                          child: Text("Log in"),
-                          style: TextButton.styleFrom(
-                            backgroundColor: Colors.white,
-                            primary: Colors.black,
-                          ),
-                        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 200,
+              ),
+              Container(
+                child: const Text(
+                  "Login",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                child: const Text(
+                  "Please login to continue life",
+                  style: TextStyle(
+                      color: Colors.white38,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                      prefixIcon: const Icon(
+                        Icons.supervised_user_circle,
+                        color: Colors.white,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(width: 1.5, color: Colors.white),
+                          borderRadius: BorderRadius.circular(10)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 1.5, color: Colors.amber),
+                          borderRadius: BorderRadius.circular(10)),
+                      border: OutlineInputBorder(),
+                      hintText: "User name",
+                      hintStyle: TextStyle(color: Colors.white30)),
+                  controller: TextEditingController(),
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextField(
+                  obscureText: true,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                      prefixIcon: const Icon(
+                        Icons.remove_red_eye_rounded,
+                        color: Colors.white,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(width: 1.5, color: Colors.white),
+                          borderRadius: BorderRadius.circular(10)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 1.5, color: Colors.amber),
+                          borderRadius: BorderRadius.circular(10)),
+                      border: OutlineInputBorder(),
+                      hintText: "*********",
+                      hintStyle: TextStyle(color: Colors.white30)),
+                  controller: TextEditingController(),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 60,
+                width: 400,
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: GestureDetector(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text("Log in"),
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.amber,
+                        primary: Colors.black,
                       ),
                     ),
                   ),
-                ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
